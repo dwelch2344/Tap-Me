332229da84571da479c5e9bb1507d0ff5ae6d518
Dave Welch <dave.welch@mokinetworks.com>
2013-10-15 19:26:56 -0600

    Initial commit

--- Tap Me/DTWViewController.h
+++ Tap Me/DTWViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DTWViewController : UIViewController
+@interface DTWViewController : UIViewController<UIAlertViewDelegate>
+
+@property IBOutlet UILabel *scoreLabel;
+@property IBOutlet UILabel *timerLabel;
+
+@property NSInteger count;
+@property NSInteger seconds;
+@property NSTimer *timer;
+
+- (IBAction)buttonPressed;
 
 @end
--- Tap Me/DTWViewController.m
+++ Tap Me/DTWViewController.m
@@ -14,16 +14,58 @@
 
 @implementation DTWViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self setupGame];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed {
+    NSLog(@"Pressed!");
+
+    
+    self.count++;
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i", self.count];
+}
+
+
+- (void)setupGame {
+    self.seconds = 30;
+    self.count = 0;
+    
+    self.timerLabel.text = [NSString stringWithFormat:@"Time: %i", self.seconds];
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score\n%i", self.count];
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(subtractTime)
+                                           userInfo:nil
+                                            repeats:YES];
+}
+
+- (void)subtractTime {
+    self.seconds--;
+    self.timerLabel.text = [NSString stringWithFormat:@"Time: %i", self.seconds];
+    
+    if (self.seconds == 0) {
+        [self.timer invalidate];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Time is up!"
+                                                        message:[NSString stringWithFormat:@"You scored %i points", self.count]
+                                                       delegate:self
+                                              cancelButtonTitle:@"Play Again"
+                                              otherButtonTitles:nil];
+        
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    [self setupGame];
+    NSLog(@"Clicked alert");
 }
 
 @end
